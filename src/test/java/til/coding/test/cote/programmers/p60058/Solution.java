3bcd2305e7f29fc11302fa48e9e785d7617d6205
Picbel <picbel.gon@gmail.com>
2022-02-06 19:39:02 +0900

    행렬 테두리 회전하기
    https://programmers.co.kr/learn/courses/30/lessons/77485

--- src/test/java/til/coding/test/cote/programmers/p60058/Solution.java
+++ src/test/java/til/coding/test/cote/programmers/p60058/Solution.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.Arrays;
+import java.util.*;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -15,21 +15,69 @@ class Solution {
     public int[] solution(int rows, int columns, int[][] queries) {
         int[] answer = new int[queries.length];
         int[][] procession = processionFactory(rows, columns);
-        for (int[] ints : procession) {
-            System.out.println(Arrays.toString(ints));
+        int answerIndex = 0;
+        for (int[] query : queries) {
+            int x1 = query[0];
+            int y1 = query[1];
+            int x2 = query[2];
+            int y2 = query[3];
+
+            List<Integer> list = new ArrayList<>();
+            //[1, 2, 2, 3]
+            for (int i = y1; i < y2; i++) {
+                list.add(getLocationValue(x1,i,procession));
+            }
+            for (int i = x1; i < x2; i++) {
+                list.add(getLocationValue(i,y2,procession));
+            }
+            for (int i = y2; i > y1; i--) {
+                list.add(getLocationValue(x2,i,procession));
+            }
+            for (int i = x2; i > x1; i--) {
+                list.add(getLocationValue(i,y1,procession));
+            }
+            answer[answerIndex] = list.stream().min(Integer::compareTo).get();
+            answerIndex++;
+            Queue<Integer> q = new LinkedList<>();
+            q.add(list.get(list.size()-1));
+            list.remove(list.size()-1);
+            q.addAll(list);
+            for (int i = y1; i < y2; i++) {
+                setLocationValue(x1,i,procession,q.poll());
+            }
+            for (int i = x1; i < x2; i++) {
+                setLocationValue(i,y2,procession,q.poll());
+
+            }
+            for (int i = y2; i > y1; i--) {
+                setLocationValue(x2,i,procession,q.poll());
+
+            }
+            for (int i = x2; i > x1; i--) {
+                setLocationValue(i,y1,procession,q.poll());
+            }
+
         }
         return answer;
     }
 
-    private int xyCalculator(int x, int y,int rows){
-        return ((x-1) * rows)+((y-1) + 1);
+    int getLocationValue(int x,int y, int[][] procession){
+//        System.out.println("x, y = "+x+" "+y);
+        return procession[x-1][y-1];
+    }
+    void setLocationValue(int x,int y ,int[][] procession, int value){
+        procession[x-1][y-1] = value;
+    }
+
+    private int xyCalculator(int x, int y,int columns){
+        return ((x-1) * columns)+y;
     }
 
     private int[][] processionFactory(int rows, int columns) {
         int[][] procession = new int[rows][columns];
         for (int x = 1; x <= rows; x++) {
             for (int y = 1; y <= columns; y++) {
-                procession[x-1][y-1] = xyCalculator(x,y,rows);
+                procession[x-1][y-1] = xyCalculator(x,y,columns);
             }
         }
         return procession;
@@ -48,5 +96,7 @@ class Solution {
               ,Arguments.of(100,97,new int[][]{{1,1,100,97}},new int[]{1})
         );
     }
+
+
 
 }
